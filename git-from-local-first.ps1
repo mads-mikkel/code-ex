60b63342b5b37922876d21c3b279c7b3ae553aa2
mads-mikkel <mara@aspit.dk>
2026-01-07 11:03:43 +0100

    added git instructions

---
+++ git-from-local-first.ps1
@@ -0,0 +1,11 @@
+cd into-new-folder
+git init
+git add .
+git commit -m "a message"
+git remote add origin https://github.com/username/repo-name.github
+git push --set-upstream origin main
+
+
+git add .
+git commit -m "new commit message"
+git push
